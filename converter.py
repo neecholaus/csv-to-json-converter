6ee90039ba73ea39f1701114810aceac8446d1f8
Nick Neuman <nick@beamingwhite.com>
2018-08-07 10:37:38 -0700

    Finished converter in python.

--- converter.py
+++ converter.py
@@ -1,11 +1,17 @@
 import sys
+import json
+import os
 
 args = sys.argv;
 
-current_file = args[1] if len(args) > 1 else 'csv.csv'
-new_file = args[2] if len(args) > 2 else 'json.json'
+current_file_name = args[1] if len(args) > 1 else 'csv.csv'
+new_file_name = args[2] if len(args) > 2 else 'json.json'
 
-file = open(current_file, 'r').read()
+try:
+	file = open(current_file_name, 'r').read()
+except: 
+	print "File was not found."
+	exit()
 
 file = file.split('\n')
 
@@ -21,13 +27,42 @@ headers_array = headers_string.split(',')
 
 rows_array = file[1:]
 
-for i in rows_array:
-	row = i.replace(',,', ', ,')
-	split_row = i.split(',')
+bad = 0
+
+master = []
+
+for i in range(len(rows_array)):
+	row = rows_array[i].replace(',,', ',"",')
+	split_row = row.split(',')
 
 	data = {}
 
-	for j in range(len(split_row)):
-		data[headers_array[j]] = split_row[j]
+	if len(headers_array) >= len(split_row):
+		for j in range(len(split_row)):
+			data[headers_array[j]] = split_row[j]
+	else:
+		bad += 1
+		continue
 
-	print data
+	master.append(data)
+
+
+new_file = open(new_file_name, 'w')
+
+try:
+	new_file.write(json.dumps(master))
+except:
+	os.remove(new_file_name)
+	print 'Something went wrong. The file was not saved.'
+	exit()
+
+
+print 'JSON was successfully saved to %s' %(new_file_name)
+
+
+
+
+
+
+
+
